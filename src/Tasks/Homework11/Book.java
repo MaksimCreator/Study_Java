0f093f817e5326fc4128a4f25d9088ea3d5f1b72
MaksimCreator <vecherkin14@mail.ru>
2026-08-18 01:10:07 +0400

    Add homework 11

---
+++ src/Tasks/Homework11/Book.java
@@ -0,0 +1,26 @@
+package Tasks.Homework11;
+
+public class Book {
+    private final String name;
+    private final Author author;
+
+    private int yearPublication;
+
+    public Book(String name, Author author, int yearPublication) {
+        this.name = name;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getName()
+    { return name; }
+
+    public Author getAuthor()
+    { return author; }
+
+    public int getYearPublication()
+    { return yearPublication; }
+
+    public void setYearPublication(int yearPublication)
+    { this.yearPublication = yearPublication; }
+}

1bda72392e130767dff6eb1d10a5dc16f89ebea5
MaksimCreator <vecherkin14@mail.ru>
2026-08-17 22:45:09 +0400

    Add homework11

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-import Homework.Homework_10;
-import Service_Locator.ServiceLocator;
-import Tasks.Homework10.*;
+import Tasks.Homework11.Author;
+import Tasks.Homework11.Book;
 
 void main()
 {
-    FirstTask firstTask = new FirstTask();
-    SecondTask secondTask = new SecondTask();
-    ThirdTask thirdTask = new ThirdTask();
+    Author authorIvan = new Author("Ivan","Ivanov");
+    Author authorPeter = new Author("Peter", "Petrov");
 
-    ServiceLocator serviceLocator = new ServiceLocator();
+    Book book1 = new Book("Rapid game",authorIvan,1963);
+    Book book2 = new Book("Start developing",authorPeter,2012);
 
-    serviceLocator.Registery(FirstTask.class,firstTask);
-    serviceLocator.Registery(SecondTask.class,secondTask);
-    serviceLocator.Registery(ThirdTask.class,thirdTask);
-
-    Homework_10 homework10 = new Homework_10(serviceLocator);
-
-    homework10.print();
+    book1.setYearPublication(2001);
 }
